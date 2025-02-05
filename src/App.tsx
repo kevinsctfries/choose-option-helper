3b716cfa8dfdb960ac96aad73a91f44884c04f4e
Kevin Fries <kevinsctfries@gmail.com>
2025-02-05 11:57:21 -0500

    Added meta tags for SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import "./App.css";
 import ChooseOption from "./Components/ChooseOption/ChooseOption";
 import Footer from "./Components/Footer/Footer";
@@ -6,6 +7,27 @@ import Header from "./Components/Header/Header";
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Choose Option Helper | OSRS Tool</title>
+        <meta
+          name="description"
+          content="Visualize how your username will appear in the 'Choose Option' menu in Old School RuneScape before creating or renaming your character."
+        />
+        <meta property="og:title" content="Choose Option Helper | OSRS Tool" />
+        <meta
+          property="og:description"
+          content="Easily preview your username in the OSRS 'Choose Option' menu."
+        />
+        <meta
+          property="og:image"
+          content="https://kevinsctfries.github.io/choose-option-helper/og-image.jpg"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://kevinsctfries.github.io/choose-option-helper/"
+        />
+      </Helmet>
       <div className="main">
         <Header />
         <ChooseOption />
